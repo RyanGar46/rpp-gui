3348e7e5378e6ce6918a9833504671154502720c
Ryan Garrett <RyanGar46@gmail.com>
2022-02-03 16:09:21 -0500

    Add column button and remove column button

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <template>
   <div class="column-container">
-    <column/>
-    <column/>
+    <div class="column-container">
+      <Column
+      v-for="(content, index) in columns"
+      :key="index"
+      :content="content"
+      :index="index"
+      />
+    </div>
+    <button class="column-button" @click="addColumn();">+</button>
   </div>
 </template>
 
 <script>
-import Column from './components/column.vue'
+import Column from "./components/Column.vue";
 export default {
   name: "App",
   components: {
     Column
+  },
+  data() {
+    return {
+      columns: {},
+      index: 0
+    }
+  },
+  methods: {
+    addColumn() {
+      this.columns["column" + this.index] = { type: "new", file: "", content: "" };
+      this.index++;
+    },
+    removeColumn(i) {
+      delete this.columns[i];
+    }
   }
 }
 </script>
@@ -20,6 +42,8 @@ export default {
   position: absolute;
   height: 100%;
   width: 100%;
+  color: white;
+  background-color: rgb(37, 37, 37);
   font-family: Helvetica, Arial, sans-serif;
 }
 
@@ -28,5 +52,6 @@ export default {
   width: 100%;
   display: flex;
   flex-direction: row;
+  overflow: auto;
 }
 </style>
---
+++ src/components/Column.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="column">
+    <div class="column-top">
+      <input type="text"/>
+      <button class="subtract-button" @click="this.$parent.removeColumn(index);">-</button>
+    </div>
+    <div class="column-content">
+      {{ content }} {{ index }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    "content",
+    "index"
+  ]
+}
+</script>
+
+<style lang="scss" scoped>
+.column {
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 auto;
+  min-width: fit-content;
+  border-radius: 8px;
+  overflow: hidden;
+  border: 2px solid rgb(37, 37, 37);
+  background-color: rgb(45, 47, 49);
+
+  .column-top,
+  .column-content {
+    padding: 8px;
+  }
+
+  .column-top {
+    border-bottom: 2px solid rgb(37, 37, 37);
+  }
+
+  .column-content {
+    flex: 1 1 auto;
+  }
+}
+
+.subtract-button {
+  height: 25px;
+  width: 25px;
+  float: right;
+}
+</style>
