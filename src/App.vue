7b5b355418c216f746579fb12fd7e655b4766ba4
Ryan Garrett <RyanGar46@gmail.com>
2022-02-03 01:38:09 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Resource Pack Packer</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  text-align: center;
+}
+</style>
